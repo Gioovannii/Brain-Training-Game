b5a98538a35c0016c5d32836e3afcb4ae8a5ddab
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-28 18:06:37 +0800

    new constant with possible choices

--- Brain Training Game/ContentView.swift
+++ Brain Training Game/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    let possibleChoices = ["Rock", "Paper", "Scissors"]
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
