16bc54928bc5c245e3e70c4dcdac4f7c1e7cbdd7
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-28 18:04:53 +0800

    Initial Commit

---
+++ Brain Training Game/Brain_Training_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_Training_GameApp.swift
+//  Brain Training Game
+//
+//  Created by Giovanni Gaffé on 2021/9/28.
+//
+
+import SwiftUI
+
+@main
+struct Brain_Training_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
